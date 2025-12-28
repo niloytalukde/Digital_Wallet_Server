06daa91aec0b9bffdb8ef03fce4e1a236a01965a
niloytalukde <niloytalukderme@gmail.com>
2025-12-28 20:28:18 +0530

    Added Vercel Porduction Link

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://digital-wallet-client-amber.vercel.app"],
     credentials: true,
   })
 );
